86b79a2ebabc666d8d50f7f1d693e9480d2ff687
Tausif4171 <tausifkhan4171@gmail.com>
2023-12-13 13:40:56 +0530

    tailwind css setup into project 🎉

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import DragAndDropFileUpload from './features/DrapAndDropFileUpload'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
       <DragAndDropFileUpload />
     </div>
   );
