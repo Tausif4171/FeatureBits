0a336bcc20484872089a645a1ae7da613690f283
Tausif4171 <tausifkhan4171@gmail.com>
2024-01-09 22:34:34 +0530

    show and mobile view

--- src/features/MobileAndDesktopView/index.tsx
+++ src/features/MobileAndDesktopView/index.tsx
@@ -5,32 +5,40 @@ import desktopIcon from '../../assets/desktopIcon.svg'
 import mobileIcon from '../../assets/mobileIcon.svg'
 
 function MobileAndDesktopView() {
+    // const [showDesktopView, setShowDesktopView] = useState(false)
 
     const [showMobileView, setShowMobileView] = useState(false)
 
     return (
-        <div>
+        <div className='flex gap-[100px]'>
+
+            {showMobileView ? <div className='flex gap-2'>
+                <h1>Hii</h1>
+                <p>How are you?</p>
+            </div> : <div className='flex flex-col gap-2'>
+                <h1>Hii</h1>
+                <p>How are you?</p>
+            </div>}
+
+
             <div>
                 <div className='h-[264px] w-[67px] flex flex-col shadow-lg rounded-[14px] bg-white'>
-                    <div className='pt-[24px] pb-[16px] px-[18.5px] border-b-[0.5px] border-[#E4E1FF] cursor-pointer'>
+                    <button className='pt-[24px] pb-[16px] px-[18.5px] border-b-[0.5px] border-[#E4E1FF] cursor-pointer'>
                         <img src={widgetIcon} />
-                    </div>
-                    <div className='py-[16px] px-[22px] border-b-[0.5px] border-[#E4E1FF] cursor-pointer'>
+                    </button>
+                    <button className='py-[16px] px-[22px] border-b-[0.5px] border-[#E4E1FF] cursor-pointer'>
                         <img src={textIcon} />
-                    </div>
-                    <div className='py-[16px] px-[18.5px] border-b-[0.5px] border-[#E4E1FF] cursor-pointer'>
+                    </button>
+                    <button className='py-[16px] px-[18.5px] border-b-[0.5px] border-[#E4E1FF] cursor-pointer'>
                         <img src={desktopIcon} />
-                    </div>
-                    <div className='py-[16px] px-[18.5px] cursor-pointer'>
+                    </button>
+                    <button className='py-[16px] px-[18.5px] cursor-pointer' onClick={() => setShowMobileView(!showMobileView)}>
                         <img src={mobileIcon} />
-                    </div>
+                    </button>
 
                 </div>
             </div>
-            <div className='flex gap-2'>
-                <h1>Hii</h1>
-                <p>How are you?</p>
-            </div>
+
         </div>
     )
 }
